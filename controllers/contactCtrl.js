60c7d6b3673776a06ad5fca11301d0ad2951742a
B-Venkata-Jitendra-reddy <venkatjitendrareddy@gmail.com>
2026-05-01 13:49:14 +0530

    models fixing into Ctrl folder

--- controllers/contactCtrl.js
+++ controllers/contactCtrl.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 const nodemailer = require("nodemailer");
 
 exports.submitContact = async (req, res) => {
